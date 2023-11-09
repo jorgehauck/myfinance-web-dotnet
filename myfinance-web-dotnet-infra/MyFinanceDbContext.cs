479f68640eaf12ec6a8bdf8eeaf45b6181abd005
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2023-11-08 21:13:39 -0300

    Configurando a Aplicação para uso do Entity Framework Core

---
+++ myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myfinance_web_dotnet_domain;
+
+namespace myfinance_web_dotnet_infra;
+
+public class MyFinanceDbContext : DbContext
+{
+
+    public DbSet<PlanoConta> PlanoConta {get; set;}
+    public DbSet<Transacao> Transacao {get; set;}
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=DESKTOP-QB722AQ\SQLEXPRESS;Database=myfinance;Trusted_Connection=True;");
+    }
+}
